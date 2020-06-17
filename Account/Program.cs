7ebbd461b238470b787b0dd792d3cbfbebb6734e
Nicolas Mendez <nicomenfede@gmail.com>
2020-06-17 22:42:24 +0100

    This really works, it's called composite enums

--- Account/Program.cs
+++ Account/Program.cs
@@ -5,11 +5,11 @@ public class Account
     [Flags]
     public enum Access
     {
-        Delete,
-        Publish,
-        Submit,
-        Comment,
-        Modify,
+        Delete = 1,
+        Publish = 2,
+        Submit = 4,
+        Comment = 8,
+        Modify = 16,
         Writer = Submit | Modify,
         Editor = Delete | Publish | Comment,
         Owner = Writer | Editor 
